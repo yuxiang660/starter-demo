79035e6c170dc823553a6095d4462f134e074dd7
yuxiang660 <yuxiang660@github.com>
2021-01-04 00:25:38 -0800

    update email demo with to lists

--- python/email/gmail.py
+++ python/email/gmail.py
@@ -2,18 +2,19 @@
 # Refer to https://realpython.com/python-send-email/
 
 import smtplib, ssl
+from email.mime.text import MIMEText
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "regression.hsv@gmail.com"  # Enter your address
-receiver_email = "yuxiangw@cadence.com"  # Enter receiver address
+receiver_emails = ["yuxiangw@cadence.com", "yuxiang660@outlook.com"]  # Enter receiver address
 password = "admin_tester"
-message = """\
-Subject: Hi there
-
-This message is sent from Python."""
+msg = MIMEText("This message is sent from Python.")
+msg['Subject'] = "subject line"
+msg['From'] = sender_email
+msg['To'] = ", ".join(receiver_emails)
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_emails, msg.as_string())
