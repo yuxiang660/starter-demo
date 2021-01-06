93dc84465379eabac73d0669efe23628b139a6ee
yuxiang660 <yuxiang660@163.com>
2021-01-06 19:29:07 +0800

    update excel example

--- python/excel/main.py
+++ python/excel/main.py
@@ -1,8 +1,43 @@
 import pandas
+import json
+import re
+
+JSON_KEY_TITLE = "title"
+JSON_KEY_PASS = "pass"
+JSON_KEY_RUN_TIME = "run_time"
+JSON_KEY_BUILD = "build"
+JSON_KEY_LOG_FILE = "log_file"
+
+CSV_KEY_COMPILE_DOMAIN = "Compile Domain"
+CSV_KEY_RELOC_DOMAIN = "Reloc Domain"
+CSV_KEY_TEST_RESULT = "Test Result"
+
+def get_compile_domain(title):
+    matches = re.findall(r"\d\S*", title)
+    assert len(matches) == 2
+    return matches[1]
+
+def get_reloc_domain(title):
+    matches = re.findall(r"\d\S*", title)
+    assert len(matches) == 2
+    return matches[0]
+
+def convert_to_csv_format(raw_data):
+    compile_domain = get_compile_domain(raw_data[JSON_KEY_TITLE])
+    reloc_domain = get_reloc_domain(raw_data[JSON_KEY_TITLE])
+    csv_data = {
+        CSV_KEY_COMPILE_DOMAIN : compile_domain,
+        CSV_KEY_RELOC_DOMAIN : reloc_domain,
+        CSV_KEY_TEST_RESULT : "Pass" if raw_data[JSON_KEY_PASS] else "Fail"
+    }
+    return csv_data
 
 if __name__ == "__main__":
-    data = pandas.read_json('input.json')
-    print(data)
-    data.to_csv("output.csv", index=False)
-    result = pandas.read_csv("output.csv")
-    print(result)
+    with open('input_comb.json', 'r') as f:
+        summary = json.load(f)
+    csv_list = []
+    for _, test_results in summary.items():
+        for raw_data in test_results:
+            csv_list.append(convert_to_csv_format(raw_data))
+    pandas.DataFrame(csv_list).to_csv('output.csv')
+
