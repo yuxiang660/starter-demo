c6d4773a06e5cb14326738eb617a7c5e5d5be0c7
yuxiang660 <yuxiang660@163.com>
2021-01-14 21:30:23 +0800

    clean python project

--- python/project/setup.py
+++ python/project/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='AP2_DevOps',
     version='0.1dev',
-    packages=['internal','tester','tester_reloc'],
+    packages=['ap2_devops'],
     license='LICENSE.txt',
     long_description=open('README.md').read(),
 )
