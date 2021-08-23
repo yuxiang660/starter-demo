7248ccc707973f828a0b0622fd09f73115855bd6
yuxiangw <yuxiangw@cadence.com>
2021-08-23 01:06:48 -0700

    update test

--- cpp/hello_vscode_debug/main.cpp
+++ cpp/hello_vscode_debug/main.cpp
@@ -2,16 +2,16 @@
 #include <string>
 #include <typeinfo>
 
+void hello(std::string s) {
+    std::cout << s << std::endl;
+    std::cout << "begin" << std::endl;
+    std::cout << "end" << std::endl;
+}
+
 int main(int argc ,char **argv)
 {
-    std::cout << "Test for CPP Syntax" << std::endl;
-    auto uint_U = 0x1FFFU;
-    auto long_uint_UL = 0x1FFFLU;
-    auto long_long_uint_UL = 0x1FFFLLU;
-
-    std::cout << "type of int_U: " << typeid(uint_U).name() << ", sizeof(U): " << sizeof(uint_U) << std::endl;
-    std::cout << "type of long_uint_UL: " << typeid(long_uint_UL).name() << ", sizeof(LU): " << sizeof(long_uint_UL) << std::endl;
-    std::cout << "type of long_long_uint_UL: " << typeid(long_long_uint_UL).name() << ", sizeof(LLU): " << sizeof(long_long_uint_UL) << std::endl;
+    std::string s("hello");
+    hello(s);
 
     return 0;
 }
