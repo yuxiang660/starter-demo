2de1286f2cff0af80802caa72c303632ce1e5de8
yuxiangw <yuxiangw@cadence.com>
2022-02-21 22:21:53 -0800

    update test

--- gdb/deadlock/main.cpp
+++ gdb/deadlock/main.cpp
@@ -18,11 +18,19 @@ int Callback()
    std::lock_guard<std::mutex> lLock(gMutex);
    return Reenter();
 }
+
+int entry()
+{
+   std::cout << "Entry...." << std::endl;
+   int ret = Callback();
+   return ret;
+}
+
 int main(int argc, char **argv)
 {
    // Prints hello message...
    std::cout << "Hello CMake World!" << std::endl;
-   std::thread lThread(Callback);
+   std::thread lThread(entry);
    lThread.join();
    std::cout << "sub thread is gone!" << std::endl;
    return 0;
