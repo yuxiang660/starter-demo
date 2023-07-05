b3954eb2492e33eef9109442cdd56d7c45eb5985
yuxiangw <yuxiangw@cadence.com>
2023-07-05 02:38:35 -0700

    add

--- cpp/str_perf/main.cpp
+++ cpp/str_perf/main.cpp
@@ -13,7 +13,9 @@ printf(str "%.6f seconds\n", std::chrono::duration_cast<std::chrono::microsecond
 #endif
 
 #define SIZE_NAME 10000
-#define LOOP_NUM 100000
+#define LOOP_NUM 10000
+
+// https://www.geeksforgeeks.org/stdstringappend-vs-stdstringpush_back-vs-operator-c/
 
 void foo()
 {
@@ -44,6 +46,51 @@ void foo2()
     }
 }
 
+void foo3()
+{
+    for (int i= 0; i < LOOP_NUM; i++)
+    {
+        std::string name;
+        name.reserve(SIZE_NAME);
+        size_t pos = 0;
+        while (pos < SIZE_NAME)
+        {
+            name += '\\';
+            pos++;
+        }
+    }
+}
+
+void foo4()
+{
+    for (int i= 0; i < LOOP_NUM; i++)
+    {
+        std::string name;
+        name.reserve(SIZE_NAME);
+        size_t pos = 0;
+        while (pos < SIZE_NAME)
+        {
+            name.push_back('\\');
+            pos++;
+        }
+    }
+}
+
+void foo5()
+{
+    for (int i= 0; i < LOOP_NUM; i++)
+    {
+        std::string name;
+        name.reserve(SIZE_NAME);
+        size_t pos = 0;
+        while (pos < SIZE_NAME)
+        {
+            name.append("\\");
+            pos++;
+        }
+    }
+}
+
 int main(int argc ,char **argv)
 {
     __TIMER_LOG("foo time: ") {
@@ -53,5 +100,18 @@ int main(int argc ,char **argv)
     __TIMER_LOG("foo2 time: ") {
         foo2();
     }
+
+    __TIMER_LOG("foo3 time: ") {
+        foo3();
+    }
+
+    __TIMER_LOG("foo4 time: ") {
+        foo4();
+    }
+
+    __TIMER_LOG("foo5 time: ") {
+        foo5();
+    }
+
     return 0;
 }
