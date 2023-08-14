997ae53c684bb7e28d7765ee57dd5ba5f9d276a2
yuxiangw <yuxiangw@cadence.com>
2023-08-14 01:34:29 -0700

    update

--- cpp/fork/main.cpp
+++ cpp/fork/main.cpp
@@ -9,54 +9,67 @@
 
 int main(int argc, char *argv[])
 {
-   int    wstatus;
-   pid_t  cpid, w;
+   int wstatus;
+   pid_t cpid, w;
 
    try
    {
       cpid = fork();
-      if (cpid == -1) {
+      if (cpid == -1)
+      {
          perror("fork");
          exit(EXIT_FAILURE);
       }
 
-      if (cpid == 0) {            /* Code executed by child */
-         printf("Child PID is %jd\n", (intmax_t) getpid());
-         if (argc == 1) sleep(1);
-            // pause();                    /* Wait for signals */
+      if (cpid == 0)
+      { /* Code executed by child */
+         printf("Child PID is %jd\n", (intmax_t)getpid());
+         if (argc == 1)
+            sleep(1);
+         // pause();                    /* Wait for signals */
          //_exit(atoi(argv[1]));
          // throw std::runtime_error("abc");
          if (chdir("abc") < 0)
             throw std::runtime_error("abc");
          exit(EXIT_SUCCESS);
-
-      } else {                    /* Code executed by parent */
-         do {
-               w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
-               if (w == -1) {
-                  perror("waitpid");
-                  exit(EXIT_FAILURE);
-               }
-               if (WEXITSTATUS(wstatus) != EXIT_SUCCESS)
-                  printf("child process error with exit code %d: %s\n", WEXITSTATUS(wstatus), strerror(errno));
-               if (WIFEXITED(wstatus)) {
-                  printf("w=%d, cpid=%d, exited, status=%d\n", w, cpid, WEXITSTATUS(wstatus));
-               } else if (WIFSIGNALED(wstatus)) {
-                  printf("killed by signal %d\n", WTERMSIG(wstatus));
-               } else if (WIFSTOPPED(wstatus)) {
-                  printf("stopped by signal %d\n", WSTOPSIG(wstatus));
-               } else if (WIFCONTINUED(wstatus)) {
-                  printf("continued\n");
-               }
+      }
+      else
+      { /* Code executed by parent */
+         do
+         {
+            w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
+            if (w == -1)
+            {
+               perror("waitpid");
+               exit(EXIT_FAILURE);
+            }
+            if (WEXITSTATUS(wstatus) != EXIT_SUCCESS)
+               printf("child process error with exit code %d: %s\n", WEXITSTATUS(wstatus), strerror(errno));
+            if (WIFEXITED(wstatus))
+            {
+               printf("w=%d, cpid=%d, exited, status=%d\n", w, cpid, WEXITSTATUS(wstatus));
+            }
+            else if (WIFSIGNALED(wstatus))
+            {
+               printf("killed by signal %d\n", WTERMSIG(wstatus));
+            }
+            else if (WIFSTOPPED(wstatus))
+            {
+               printf("stopped by signal %d\n", WSTOPSIG(wstatus));
+            }
+            else if (WIFCONTINUED(wstatus))
+            {
+               printf("continued\n");
+            }
          } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
          exit(EXIT_SUCCESS);
       }
    }
-   catch(const std::exception &e)
+   catch (const std::exception &e)
    {
       std::cout << "exception " << e.what() << std::endl;
    }
-   catch(...)
+   catch (...)
    {
       std::exception_ptr p = std::current_exception();
       std::cout << "Unexpected error happens when reading SPEF files: " << (p ? p.__cxa_exception_type()->name() : "null") << "\n";
